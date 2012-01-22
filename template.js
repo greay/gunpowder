6b63dd29b3b15fccda49bdf780dc268bd0d49233
Caleb Cohoon <calebcohoon@gmail.com>
2012-01-22 12:48:10 -0800

    updating template.js

--- template.js
+++ template.js
@@ -1,9 +1,15 @@
-class MySceneSpec extends gunpowder {
+class MySceneSpec extends Gunpowder {
   function run() {
     describe('my game tests', function() {
       beforeEach(function() {
         loadScene('your scene name here');
       });
+      
+      describe('when the game starts', function() {
+        it('should not show my game object', function() {
+          expect(_('my game object name')).toBeHidden();
+        });
+      })
     });
   }
 }
